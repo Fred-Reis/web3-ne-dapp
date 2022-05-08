cc9f7f7244e3998ea07bd79dae6da883a7dd7ecc
Fred-Reis <fredgost@hotmail.com>
2022-05-07 23:22:20 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import './style.css';
 
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Please install Metamask!');
+      } else {
+        console.log('We have Ethereum object', ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log('Authorized account has found', account);
+        setCurrentAccount(account);
+      } else {
+        console.log('Nothing account authorized has found');
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Metamask has found!');
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    // console.log('ethers', ethers);
+    checkIfWalletIsConnected();
+  }, []);
+
   const wave = () => {
     console.log('oi');
   };
@@ -20,6 +71,12 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           🙋🏻‍♀️🙋‍♀️🙋‍♀️🙋🏽‍♀️🙋🏽‍♀️🙋🏿‍♀️ Let's wave for people 🙋🏼‍♂️🙋🏻‍♂️🙋🏽‍♂️🙋🏾‍♂️🙋🏿‍♂️🙋🏻‍♂️
         </button>
+
+        {!currentAccount && (
+          <button className="connectWalletButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
